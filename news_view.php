9d35d4c6bdcd2d2d1a6b042d143f0397a3a33212
Liz <stoneliz@users.noreply.github.com>
2017-03-14 20:56:39 -0700

    PSR Standards Compliant

--- news_view.php
+++ news_view.php
@@ -1,10 +1,7 @@
 <?php
 /**
- * survey_view.php is a page to demonstrate the proof of concept of the 
- * initial SurveySez objects.
+ * news_view.php 
  *
- * Objects in this version are the Survey, Question & Answer objects
- * 
  * @package NewsAggregator
  * @author Group 7 Anthony Stenberg-Di Geronimo, Kiara McMorris, Liz Stone, Yan Men
  * @version 2.10 2017/03/09
@@ -27,15 +24,13 @@ if(isset($_GET['id']) && (int)$_GET['id'] > 0){#proper data must be on querystri
 	myRedirect(VIRTUAL_PATH . "news/index.php");
 }
 $myNews = new NewsFeedz\News($myID); //MYNews extends survey class so methods can be added
-if($myNews->isValid)
-{
+if($myNews->isValid){
 	$config->titleTag = "'" . $myNews->Title . "' News feed!";
 }else{
 	$config->titleTag = smartTitle(); //use constant 
 }
 
-if(isset($_GET['id']))
-{//process data
+if(isset($_GET['id'])){//process data
     //cast the data to an integer, for security purposes
     $id = (int)$_GET['id'];
 }else{//redirect to safe page
@@ -53,36 +48,31 @@ get_header(); #defaults to theme header or header_inc.php
  //  IDB::conn() creates a shareable database connection via a singleton class
  $result = mysqli_query(IDB::conn(),$sql) or die(trigger_error(mysqli_error(IDB::conn()), E_USER_ERROR));
 
- if(mysqli_num_rows($result) > 0)
-{#there are records - present data
+ if (mysqli_num_rows($result) > 0){#there are records - present data
     // ** This Echo statement sets up the table. It also includes classes from the rest of the protosite to maintain a similar look and feel. 
-    echo'<table class="table table-striped table-hover ">
-  <thead>
-    <tr>
-      <th>Title</th>
-      <th>Description</th>
-    </tr>
-  </thead>
-  <tbody>';
+      echo'<table class="table table-striped table-hover ">
+           <thead>
+             <tr>
+               <th>Title</th>
+               <th>Description</th>
+             </tr>
+           </thead>
+           <tbody>';
     // ** This while loop Spits out the rows based on the Variable $result.
-	while($row = mysqli_fetch_assoc($result))
-	{# pull data from associative array
+    while ($row = mysqli_fetch_assoc($result)){# pull data from associative array
 	  
         echo '<tr>
-      <td><a href="feed_view.php?id=' . $row['NewsID'] . '">' . $row['NewsTitle'] . '</a></td>  
-      <td>' . $row['Description'] . '</td>
-    </tr>';
+                <td><a href="feed_view.php?id=' . $row['NewsID'] . '">' . $row['NewsTitle'] . '</a></td>  
+                <td>' . $row['Description'] . '</td>
+              </tr>';
 	}
     // ** This closes the table. 
     echo '  </tbody>
-</table> ';
+            </table> ';
     
-}else{#no records
+}else {#no records
 	echo '<div align="center">Sorry, there are no records that match this query</div>';
 }
-
-
-
 
 
 get_footer(); #defaults to theme footer or footer_inc.php
